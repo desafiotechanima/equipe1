73232b3dd0dc47442e3cd1fcbc07c0afee32dda7
marcusvsfreitas <marcus.sfreitas@outlook.com>
2022-04-30 15:35:08 -0300

    Adiciona menu de botões

--- desafio-tech/src/views/Home.vue
+++ desafio-tech/src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row text-white">
+      <div class="col-12 col-md-6">
+        <div style="border-radius: 5px; background: linear-gradient(98.96deg, #548AD8 16.92%, #8A4BD3 85.29%)" class="mt-2 d-flex justify-content-center align-items-center">
+          <span class="fs-5">
+            Minecraft
+            <br>
+            <span class="fs-6">
+             (Ciências da Natureza)
+            </span>
+          </span>
+        </div>
+      </div>
+      <div class="col-12 col-md-6">
+        <div style="border-radius: 5px; background: linear-gradient(98.96deg, #548AD8 16.92%, #8A4BD3 85.29%)" class="mt-2 d-flex justify-content-center align-items-center">
+          <span class="fs-5">
+            Roblox
+            <br>
+            <span class="fs-6">
+             (Lógica de Programação)
+            </span>
+          </span>
+        </div>
+      </div>
+      <div class="col-12 col-md-6">
+        <div style="border-radius: 5px; background: linear-gradient(98.96deg, #893E9C 16.92%, #F82B73 85.29%)" class="mt-2 d-flex justify-content-center align-items-center">
+          <span class="fs-5">
+            Perguntados
+            <br>
+            <span class="fs-6">
+              (Ciências Humanas)
+            </span>
+          </span>
+          
+        </div>
+      </div>
+      <div class="col-12 col-md-6">
+          <div style="border-radius: 5px; background: linear-gradient(98.96deg, #2F68D7 16.92%, #F79334 85.29%)" class="mt-2 d-flex justify-content-center align-items-center">
+          <span class="fs-5">
+            Show do Milhão
+            <br>
+            <span class="fs-6">
+              (Atualidades)
+            </span>
+          </span>
+          
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
