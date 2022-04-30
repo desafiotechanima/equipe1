a027d661efc54d8b39c70715ac2a624eb87f0116
marcusvsfreitas <marcus.sfreitas@outlook.com>
2022-04-30 14:27:25 -0300

    Adiciona tela de login

--- desafio-tech/src/App.vue
+++ desafio-tech/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light"  v-if="$route.name != 'Login'">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
           <img src="https://admanalisa.genialinvestimentos.com.br/wp-content/uploads/2021/03/logo-anima-02.png" width="160px" height="40px">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -14,7 +14,7 @@
               <a class="nav-link active" aria-current="page" href="#">Sobre</a>
             </li>
           </ul>
-        </div>
+        </div> -->
       </div>
     </nav>
 
--- desafio-tech/src/views/Login.vue
+++ desafio-tech/src/views/Login.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="login">
-    
+    <div class="row mt-4">
+      <h6 class="text-center">Um espaço lúdico e educacional para o desenvolvimento do seu filho</h6>
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label d-flex justify-content-start">Usuário</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite o nome de usuário">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label d-flex justify-content-start">Senha</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Digite a sua senha">
+        </div>
+        <div class="d-flex justify-content-end">
+            <router-link to="/about">
+              <button type="button" class="btn text-white btn_gradient_1">Entrar</button>
+            </router-link>
+        </div>
+
+        <div> Ou </div>
+        <button type="button" class="mt-2 btn text-white  btn_gradient_2">Crie seu login</button>
+      </form>
+    </div>
   </div>
 </template>
 
