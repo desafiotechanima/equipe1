85fd770b8ce6af8c41d8c738f6f41973fcb87c57
marcusvsfreitas <marcus.sfreitas@outlook.com>
2022-04-30 14:05:19 -0300

    Adiciona tela de sobre

--- desafio-tech/src/App.vue
+++ desafio-tech/src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light"  v-if="$route.name != 'Login'">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="https://admanalisa.genialinvestimentos.com.br/wp-content/uploads/2021/03/logo-anima-02.png" width="160px" height="40px">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Sobre</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style>
+@import "./style.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- desafio-tech/src/views/About.vue
+++ desafio-tech/src/views/About.vue
@@ -1,5 +1,71 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 mt-4">
+          <h1>Oi {{ user.name }},</h1>
+          <p>Sabemos do desafio do estudo EAD com os filhos em casa. Nessa plataforma enquanto você estuda seus filhos se divertem com jogos educacionals desenvolvidos pelos nossos professores.</p>
+        </div>
+        <div class="col-12 d-flex justify-content-center justify-content-md-end">
+          <button type="button" class="btn text-white btn_gradient_1 me-4">Seu perfil</button>
+          <button type="button" class="btn text-white  btn_gradient_2">Seus filhos</button>
+        </div>
+        <div class="col-12 mt-4">
+          <div class="d-flex flex-column text-white position-absolute px-4 py-3 w-100">
+            <div class="text-start">
+              <h1>
+                Escolha
+              </h1>
+              <p class="w-50">
+                os conteúdos educacionals de maior defasagem para seus fihos treinarem enquanto você estuda
+              </p>
+            </div>
+            <div class="text-start" style="margin-top: 60px">
+              <h1>
+                Brinque
+              </h1>
+              <p>
+                e acumule pontos
+              </p>
+            </div>
+            <div class="text-start" style="margin-top: 60px">
+              <h1>
+                Troque
+              </h1>
+              <p>
+                seus pontos por jogos nas empresas parceiras
+              </p>
+            </div>
+          </div>
+          <img class="img-about w-100" src="@/assets/background-about.png" alt="Fundo colorido e com formas circulares">
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Login',
+  data: () => ({
+    user: {
+      name: "Lilian"
+    }
+  }),
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+@media (min-height: 600px) {
+  .img-about {
+    object-fit: cover;
+    border-radius: 15px;
+    height: 500px;
+  }
+}
+</style>
+
